406dc0c6da8ce36243295e3ca3bdbb163fee21ae
piyush bohra <pbohra134@gmail.com>
2026-02-15 14:15:22 +0530

    using js and adjusting nav

--- lazarev.js
+++ lazarev.js
@@ -1,8 +1,39 @@
 var nav = document.querySelector("nav")
 
 nav.addEventListener("mouseenter",function(){
-    console.log("hello")
-});
+    let t1 = gsap.timeline()
+    
+    t1.to("#nav-bottom",{
+        height:"21vh"
+    })
+    t1.to(".nav-part2 h5 ",{
+    display:"block"
+    })
+    t1.to(".nav-part2 h5 span",{
+    y:25,
+    /*duration:0.3;*/
+    stagger:{
+        amount:0.1
+    }
+    })
+})
+nav.addEventListener("mouseleave",function(){
+    var t1 = gsap.timeline()
+    
+   /* t1.to("#nav-bottom",{
+        height:"21vh"
+    })
+    t1.to(".nav-part2 h5 ",{
+    display:"block"
+    })*/
+    t1.to(".nav-part2 h5 span",{
+    y:25,
+    /*duration:0.3;*/
+    stagger:{
+        amount:0.2
+    }
+    })
+})
 
 
 
