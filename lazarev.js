dae0a950cc231b9b2257b5db892676eff112d3ed
piyush bohra <pbohra134@gmail.com>
2026-03-07 14:05:06 +0530

    working with video play section

--- lazarev.js
+++ lazarev.js
@@ -113,6 +113,20 @@ rightElems.forEach(function(elem){
         })
     })
 })
+var page3Center =document.querySelector(".page3-center")
+var video = document.querySelector("#page3 video")
+
+page3Center.addEventListener("click",function(){
+    video.play()
+    gsap.to(video,{
+        transform:"scaleX(1) scaleY(1)",
+        opacity:1
+
+    })
+})
+video.addEventListener("click",function(){
+    video.pause()
+})
 
 
 
