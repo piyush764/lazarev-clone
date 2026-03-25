16d5c830cba46cbc30175976af8d068d932ddc9c
piyush bohra <pbohra134@gmail.com>
2026-03-25 21:31:12 +0530

    mistake

--- lazarev.js
+++ lazarev.js
@@ -133,18 +133,19 @@ sections.forEach(function(elem){
 //   <---page6-animation--->
 //1. Register plugin
 gsap.registerPlugin(ScrollTrigger);
-
-gsap.from("#btm6-part2 h4",{
-    x:(i) => i*50,
+["#btm6-part2", "#btm6-part3", "#btm6-part4"].forEach((col, index) => {
+gsap.from(`${col} h4`,{
+    x:(i) => i*40,
+   // opacity:1,
     duration:1,
     scrollTrigger:{
-        trigger:"#btm6-part2",
-       // scroller:"body",
-        markers:true,
+        trigger:col,
+        markers:false,
         start:"top 80%",
         end:"top 20%",
         scrub:true
     }
+});
 });
 
 
